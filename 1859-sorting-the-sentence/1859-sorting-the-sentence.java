ab0ffe5af8fdd4a1b3b390077885aa873b76c027
kanikasekar2006 <sekarkanika@gmail.com>
2026-07-24 05:42:14 +0530

    Time: 1 ms (39.18%), Space: 42.8 MB (65.42%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.java
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.java
@@ -4,17 +4,17 @@ class Solution {
         String str1[]=new String[str.length];
         
         for(String word:str){
-            String sb="";
+           StringBuilder sb = new StringBuilder();
             int i=0;
         for(char ch:word.toCharArray()){
             if(Character.isLetter(ch)){
-                sb=sb+ch;
+                sb.append(ch);
             }
             else if(Character.isDigit(ch)){
                 i=ch-'0';
             }
         }
-        str1[i-1]=sb;
+        str1[i-1]=sb.toString();
     }
     return String.join(" ",str1);
 }
