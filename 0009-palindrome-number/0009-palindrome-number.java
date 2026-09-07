047ebe67684d224116c0bb7dbd1b3175470b7ccf
kanikasekar2006 <sekarkanika@gmail.com>
2026-09-07 14:51:06 +0530

    Time: 5 ms (82.41%), Space: 44 MB (99.98%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0||(x%10==0&&x!=0)){
+            return false;
+        }
+        int temp=x;
+        int rev=0;
+        while(temp>rev){
+            int digit=temp%10;
+            rev=rev*10+digit ;
+            temp=temp/10;
+        }
+        
+        return temp==rev||temp==rev/10 ;
+    }
+}
